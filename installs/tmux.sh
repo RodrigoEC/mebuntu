4c381db90bbe7ed7e799f3aa3628535ed77c7aa4
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-31 17:04:06 -0300

    Adds tmux and tmuxinator installation script

---
+++ installs/tmux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source ./colors.sh
+
+install_tmux() {
+	sudo apt update
+	echo -e "\n${YELLOW}> Starting to install TMUX${NC}"
+	sudo apt install tmux
+
+	echo -e "\n${YELLOW}> Starting to install tmuxinator${NC}"
+	sudo apt install tmuxinator
+}
+
+install_tmux 2>../logs/tmux_errors.txt
+if [ $? -eq 0 ]
+then
+	echo -e "\n${GREEN}> TMUX and Tmuxinator installed successfully${NC}"
+else
+	echo -e "\n${RED}> Error when installing TMUX or tmuxinator, got to logs/tmux_errors.txt to see what went wrong${NC}"
+fi
