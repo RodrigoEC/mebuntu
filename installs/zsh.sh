6a62fb452daba330e4889df20df0f3f92169479f
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-31 17:43:48 -0300

    Adds zsh installation scrip

---
+++ installs/zsh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source ./colors.sh
+
+
+install_zsh() {
+	sudo apt update
+	echo -e "\n${YELLOW}> Starting to install ZSH${NC}"
+	sudo apt install zsh -y
+	echo -e "\n${YELLOW}> Starting to install oh-my-zsh${NC}"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+install_zsh 2>../logs/zsh_errors.txt
+if [ $? -eq 0 ]
+then
+	echo -e "\n${GREEN}> ZSH and oh-my-zsh installed successfully${NC}"
+else
+	echo -e "\n${RED}> Error when installing ZSH and oh-my-zsh, got to logs/zsh_errors.txt to see what went wrong${NC}"
+fi
+
